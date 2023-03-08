322760009774290d75abae0f8fbc25f98cfff5a9
matthew <larochelle.matthew@gmail.com>
2023-03-08 15:22:39 -0800

    First commit

---
+++ src/components/queries/Queries.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import styles from './Queries.module.css';
+
+function Queries() {
+    return (
+        <div>
+            <h1>Search Results:</h1>
+            <p>ABCDEFU</p>
+            <p>A Thousand Years</p>
+            <p>About A Girl</p>
+            <p>All My Favorite Songs</p>
+            <p>All The Small Things</p>
+            <p>Alone</p>
+            <p>Babe I'm Gonna Leave You</p>
+            <p>Backyard Boy</p>
+            <p>Bad Habits</p>
+            <p>Black Sheep</p>
+            <p>Blackbird</p>
+            <p>Blackbird Song</p>
+            <p>Boulevard Of Broken Dreams</p>
+            <p>Burn</p>
+            <p>Come As You Are</p>
+            <p>Crazy</p>
+            <p>Crazy Little Thing Called Love</p>
+            <p>Dandelions</p>
+            <p>Down To The River To Pray</p>
+            <p>Dragostea Din Te</p>
+            <p>Electric Love</p>
+            <p>Ettrick Lady</p>
+            <p>Famous Blue Raincoat</p>
+            <p>Feel Good Inc</p>
+            <p>Fields Of Gold</p>
+            <p>Firework</p>
+            <p>First Love Late Spring</p>
+            <p>Fix You</p>
+            <p>Francis Forever</p>
+            <p>Free</p>
+            <p>Get Outta My Way</p>
+            <p>Girls Like You</p>
+            <p>Good Riddance</p>
+            <p>Goodbye Yellow Brick Road</p>
+            <p>Gorgeous</p>
+            <p>Happy Birthday</p>
+            <p>Havana</p>
+            <p>Heart Attack</p>
+            <p>Help I'm Alive</p>
+            <p>Hey Soul Sister</p>
+            <p>Hold Me While You Wait</p>
+            <p>Hurt</p>
+            <p>I Will Survive</p>
+            <p>I'll Fly Away</p>
+            <p>Imagine</p>
+            <p>Immigrant Song</p>
+            <p>It Ain't Me</p>
+            <p>Knicking On Heavens Door</p>
+            <p>Leave Out All The Rest</p>
+            <p>Levitating</p>
+            <p>Losing You</p>
+            <p>Love Like You</p>
+            <p>Love Me Do</p>
+            <p>Lover</p>
+            <p>Memories</p>
+            <p>Midnight Train To Memphis</p>
+            <p>Minestroni Macaroni</p>
+            <p>Middle of Starting Over</p>
+            <p>Minority</p>
+            <p>Monody</p>
+            <p>Mood</p>
+            <p>More Of You</p>
+            <p>Motion Sickness</p>
+            <p>My Heart Will Go On</p>
+            <p>My Immortal</p>
+        </div>
+    );
+}
+
+export default Queries;
