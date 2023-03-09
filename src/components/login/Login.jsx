d19d551aed1abb53ebc11f8aa0de26db35483a77
matthew <larochelle.matthew@gmail.com>
2023-03-09 11:40:47 -0800

    slight adjustments

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -6,8 +6,8 @@ function Login() {
         <div className={styles.container}>
             <h1>Log In</h1>
             <form>
-                <input type='text' name="username"></input>
-                <input type='password' name='password'></input>
+                <input type='text' name="username" placeholder="Username"></input>
+                <input type='password' name='password' placeholder="Password"></input>
                 <button type='submit'>Log In</button>
             </form>
         </div>
--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -5,29 +5,31 @@ function Profile() {
     return (
         <div className={styles.container}>
             <h1>Profile</h1>
-            <div className={styles.mastered}>
-                <h2>Songs Mastered:</h2>
-                <ul>
-                    <li>Song</li>
-                    <li>Song</li>
-                    <li>Song</li>
-                </ul>
-            </div>
-            <div className={styles.working}>
-                <h2>Currently Working:</h2>
-                <ul>
-                    <li>Song</li>
-                    <li>Song</li>
-                    <li>Song</li>
-                </ul>
-            </div>
-            <div className={styles.dream}>
-                <h2>Dream Songs:</h2>
-                <ul>
-                    <li>Song</li>
-                    <li>Song</li>
-                    <li>Song</li>
-                </ul>
+            <div className={styles.songs}>
+                <div className={styles.mastered}>
+                    <h2>Songs Mastered:</h2>
+                    <ul>
+                        <li>Song</li>
+                        <li>Song</li>
+                        <li>Song</li>
+                    </ul>
+                </div>
+                <div className={styles.working}>
+                    <h2>Currently Working:</h2>
+                    <ul>
+                        <li>Song</li>
+                        <li>Song</li>
+                        <li>Song</li>
+                    </ul>
+                </div>
+                <div className={styles.dream}>
+                    <h2>Dream Songs:</h2>
+                    <ul>
+                        <li>Song</li>
+                        <li>Song</li>
+                        <li>Song</li>
+                    </ul>
+                </div>
             </div>
             <div className={styles.log}>
                 <h2>Practice Log:</h2>
