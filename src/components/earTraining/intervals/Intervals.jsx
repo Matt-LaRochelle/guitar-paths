77ec36c6c0d493602e26a3e1e1d28b4625ece9d8
matthew <larochelle.matthew@gmail.com>
2023-04-18 12:32:33 -0700

    create intervals and add to page

--- src/components/earTraining/intervals/Intervals.jsx
+++ src/components/earTraining/intervals/Intervals.jsx
@@ -10,19 +10,28 @@ import intervals from '../noteSounds/intervalSounds';
 function Intervals() {
     const audioClips = audioClip;
 
-    function min2() {
-        const note = new Audio(audioClips[0].clip);
+    // Plays the two note interval
+    function interval(e) {
+        const note = new Audio(audioClips[5].clip);
+
+        // Turn button ID into number for dynamic intervals
+        let n = (5 + Number(e.target.id))
         note.play();
         setTimeout(() => {
-            const note2 = new Audio(audioClips[1].clip);
+            const note2 = new Audio(audioClips[n].clip);
             note2.play();
         }, 1000); 
     }
 
-    function jaws() {
-        const sample = new Audio(intervals[0].clip);
+    // Plays brief clip of interval in context of song
+    function context(e) {
+        // Turn button ID into number for dynamic intervals
+        let n = Number(e.target.id);
+        const sample = new Audio(intervals[n].clip);
         sample.play();
     }
+
+
     return (
         <div className={styles.container}>
             <h2>Intervals</h2>
@@ -30,8 +39,8 @@ function Intervals() {
             <ul>
                 <li className={styles.li}>
                     <p>Minor 2nd: Jaws</p>
-                    <button className={styles.btn} onClick={min2}>Interval Sound</button>
-                    <button className={styles.btn} onClick={jaws}>Interval in Context</button>
+                    <button className={styles.btn} id="1" onClick={interval}>Interval Sound</button>
+                    <button className={styles.btn} id="0" onClick={context}>Interval in Context</button>
                     <iframe 
                         width="200" 
                         height="200" 
@@ -42,18 +51,63 @@ function Intervals() {
                         allowfullscreen
                         >
                     </iframe>
-                    <img className={styles.diagram} src={m2} alt="diagram" /></li>
-                <li>Major 2nd: Happy Birthday</li>
-                <li>Minor 3rd: Greensleeves</li>
-                <li>Major 3rd: Oh When The Saints</li>
-                <li>Perfect 4th: Wedding March</li>
-                <li>Tritone: The Simpsons</li>
-                <li>Perfect 5th: Star Wars</li>
-                <li>Minor 6th: Go Down Moses</li>
-                <li>Major 6th: My Bonnie Lies Over The Ocean</li>
-                <li>Minor 7th: Somewhere</li>
-                <li>Major 7th: Take On Me</li>
-                <li>Perfect Octave: Somewhere Over The Rainbow</li>
+                    <img className={styles.diagram} src={m2} alt="diagram" />
+                </li>
+                <li className={styles.li}>
+                    <p>Major 2nd: Happy Birthday</p>
+                    <button className={styles.btn} id="2" onClick={interval}>Interval Sound</button>
+                    <button className={styles.btn} id="1" onClick={context}>Interval in Context</button>
+                </li>
+                <li>
+                    <p>Minor 3rd: Greensleeves</p>
+                    <button className={styles.btn} id="3" onClick={interval}>Interval Sound</button>
+                    <button className={styles.btn} id="2" onClick={context}>Interval in Context</button>
+                </li>
+                <li>
+                    <p>Major 3rd: Oh When The Saints</p>
+                    <button className={styles.btn} id="4" onClick={interval}>Interval Sound</button>
+                    <button className={styles.btn} id="3" onClick={context}>Interval in Context</button>
+                </li>
+                <li>
+                    <p>Perfect 4th: Wedding March</p>
+                    <button className={styles.btn} id="5" onClick={interval}>Interval Sound</button>
+                    <button className={styles.btn} id="4" onClick={context}>Interval in Context</button>
+                </li>
+                <li>
+                    <p>Tritone: The Simpsons</p>
+                    <button className={styles.btn} id="6" onClick={interval}>Interval Sound</button>
+                    <button className={styles.btn} id="5" onClick={context}>Interval in Context</button>
+                </li>
+                <li>
+                    <p>Perfect 5th: Star Wars</p>
+                    <button className={styles.btn} id="7" onClick={interval}>Interval Sound</button>
+                    <button className={styles.btn} id="6" onClick={context}>Interval in Context</button>
+                </li>
+                <li>
+                    <p>Minor 6th: Go Down Moses</p>
+                    <button className={styles.btn} id="8" onClick={interval}>Interval Sound</button>
+                    <button className={styles.btn} id="7" onClick={context}>Interval in Context</button>
+                </li>
+                <li>
+                    <p>Major 6th: My Bonnie Lies Over The Ocean</p>
+                    <button className={styles.btn} id="9" onClick={interval}>Interval Sound</button>
+                    <button className={styles.btn} id="8" onClick={context}>Interval in Context</button>
+                </li>
+                <li>
+                    <p>Minor 7th: Somewhere</p>
+                    <button className={styles.btn} id="10" onClick={interval}>Interval Sound</button>
+                    <button className={styles.btn} id="9" onClick={context}>Interval in Context</button>
+                </li>
+                <li>
+                    <p>Major 7th: Take On Me</p>
+                    <button className={styles.btn} id="11" onClick={interval}>Interval Sound</button>
+                    <button className={styles.btn} id="10" onClick={context}>Interval in Context</button>
+                </li>
+                <li>
+                    <p>Perfect Octave: Somewhere Over The Rainbow</p>
+                    <button className={styles.btn} id="12" onClick={interval}>Interval Sound</button>
+                    <button className={styles.btn} id="11" onClick={context}>Interval in Context</button>
+                </li>
             </ul>
             <h4>Descending:</h4>
             <ul>
--- src/components/earTraining/noteSounds/intervalSounds.jsx
+++ src/components/earTraining/noteSounds/intervalSounds.jsx
@@ -1,8 +1,77 @@
 import jaws from '../../sounds/interval_notes/jaws.mp3';
+import happyBirthday from '../../sounds/interval_notes/happyBirthday.mp3';
+import greenSleeves from '../../sounds/interval_notes/greenSleeves.mp3';
+import ohWhenTheSaints from '../../sounds/interval_notes/ohWhenTheSaints.mp3';
+import weddingMarch from '../../sounds/interval_notes/weddingMarch.mp3'
+import simpsons from '../../sounds/interval_notes/simpsons.mp3'
+import starWars from '../../sounds/interval_notes/starWars.mp3'
+import goDownMoses from '../../sounds/interval_notes/goDownMoses.mp3'
+import myBonnie from '../../sounds/interval_notes/myBonnie.mp3'
+import somewhere from '../../sounds/interval_notes/somewhere.mp3'
+import takeOnMe from '../../sounds/interval_notes/takeOnMe.mp3'
+import somewhereOverTheRainbow from '../../sounds/interval_notes/somewhereOverTheRainbow.mp3'
 
 const intervals = [
-    {"clip": jaws},
-    {"interval": "minor 2nd"}
+    {
+        "clip": jaws,
+        "interval": "minor 2nd"
+    },
+
+    {
+        "clip": happyBirthday,
+        "interval": "major 2nd"
+    },
+
+    {
+        "clip": greenSleeves,
+        "interval": "minor 3rd"
+    },
+
+    {
+        "clip": ohWhenTheSaints,
+        "interval": "major 3rd"
+    },
+
+    {
+        "clip": weddingMarch,
+        "interval": "perfect 4th"
+    },
+
+    {
+        "clip": simpsons,
+        "interval": "tritone"
+    },
+
+    {
+        "clip": starWars,
+        "interval": "perfect 5th"
+    },
+
+    {
+        "clip": goDownMoses,
+        "interval": "minor 6th"
+    },
+
+    {
+        "clip": myBonnie,
+        "interval": "major 6th"
+    },
+
+    {
+        "clip": somewhere,
+        "interval": "minor 7th"
+    },
+
+    {
+        "clip": takeOnMe,
+        "interval": "major 7th"
+    },
+
+    {
+        "clip": somewhereOverTheRainbow,
+        "interval": "perfect 8th"
+    }
+
 ]
 
 export default intervals;
