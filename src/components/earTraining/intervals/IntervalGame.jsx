7e7655ce50affdc41e971d8c60084cb3159e5a7d
matthew <larochelle.matthew@gmail.com>
2023-04-21 08:59:35 -0700

    refactor code for game

--- src/components/earTraining/intervals/IntervalGame.jsx
+++ src/components/earTraining/intervals/IntervalGame.jsx
@@ -8,7 +8,8 @@ function IntervalGame() {
 
     //Individual sound clips
     const audioClips = audioClip;
-    //Game logic without additional settings
+
+    //Foundational game logic WAS 10 and 22 p8, 12
     const [note1, setNote1] = useState(audioClips[10].clip);
     const [note2, setNote2] = useState(audioClips[22].clip);
     const [interval, setInterval] = useState(12);
@@ -20,80 +21,62 @@ function IntervalGame() {
     const [gotAnswer, setGotAnswer] = useState(null);
 
     //Additional settings
-    const [ascending, setAscending] = useState(false);
-    const [descending, setDescending] = useState(false);
-    const [melodic, setMelodic] = useState(false);
-    const [harmonic, setHarmonic] = useState(false);
+    const [ascending, setAscending] = useState("On");
+    const [descending, setDescending] = useState("Off");
+    const [melodic, setMelodic] = useState("On");
+    const [harmonic, setHarmonic] = useState("Off");
 
-    function playInterval() {
-        if (melodic === true && harmonic === true) {
-            // alert("How do I do this?")
-            let randomChoice = Math.round(Math.random());
-            if (randomChoice) {
-                const first_note = new Audio(note1);
-                first_note.play();
-                setTimeout(() => {
-                    const second_note = new Audio(note2);
-                    second_note.play();
-                }, 1000);
-            } else if (!randomChoice) {
-                const first_note = new Audio(note1);
-                first_note.play();
-                const second_note = new Audio(note2);
-                second_note.play();
-            }
-            
-
-        } else if (melodic === true) {
-            const first_note = new Audio(note1);
-            first_note.play();
-            setTimeout(() => {
-                const second_note = new Audio(note2);
-                second_note.play();
-            }, 1000);
-
-        } else if (harmonic === true) {
-            const first_note = new Audio(note1);
-            first_note.play();
-            const second_note = new Audio(note2);
-            second_note.play();
-        } else if (harmonic !== true && melodic !==true) {
-            alert("You must have either melodic or harmonic selected, or both")
-        } else if (ascending !==true && descending !==true) {
-            alert("You must have either ascending or descending selected or both!")
-        }
-    }
-
-
-    //Function for logging what is written in input
-    function handleChange(event) {
-        const newValue = event.target.value;
-        setInputText(newValue);
-        // console.log(inputText)
-    }
+    //If true: interval is melodic, else if false: interval is harmonic
+    const [melodicOrHarmonic, setMelodicOrHarmonic] = useState(false);
 
     function settings(e) {
         if (e.target.id === "ascending") {
-            setAscending(!ascending)
+            if (ascending === "Off") {
+                setAscending("On");
+            } else if (ascending === "On") {
+                setAscending("Off");
+            } else {
+                console.log(ascending);
+            }
         } else if (e.target.id === "descending") {
-            setDescending(!descending)
+            if (descending === "Off") {
+                setDescending("On");
+            } else if (descending === "On") {
+                setDescending("Off");
+            } else {
+                console.log(descending);
+            }
         } else if (e.target.id === "melodic") {
-            setMelodic(!melodic)
+            if (melodic === "Off") {
+                setMelodic("On");
+            } else if (melodic === "On") {
+                setMelodic("Off");
+            } else {
+                console.log(melodic);
+            }
         } else if (e.target.id === "harmonic") {
-            setHarmonic(!harmonic)
+            if (harmonic === "Off") {
+                setHarmonic("On");
+            } else if (harmonic === "On") {
+                setHarmonic("Off");
+            } else {
+                console.log(harmonic);
+            }
         }
-        console.log(
-            "Ascending: ", ascending,
-            "Descending: ", descending,
-            "Melodic: ", melodic,
-            "Harmonic :", harmonic
-        )
+        //Generate a new interval, notes, mel/harm - I chained them together
+        newInterval();
+
+        // console.log(
+        //     "Ascending: ", ascending,
+        //     "Descending: ", descending,
+        //     "Melodic: ", melodic,
+        //     "Harmonic :", harmonic
+        // )
     }
 
-    function check_answer() {
-        if (inputText === answer) {
-            //Get a new set of notes/interval/answers
+    function newInterval() {
         //Set interval between 1-12
+        //Then set the answer according to randomly chosen interval
         setInterval(Math.floor(Math.random() * 12) + 1)
         if (interval === 1) {
             setAnswer("m2");
@@ -120,9 +103,13 @@ function IntervalGame() {
         } else if (interval === 12) {
             setAnswer("p8");
         }
+        //Generate new notes
+        newNotes();
+    }
 
+    function newNotes() {
         //Set N1 between 0-36 (No +1 because list of sounds goes from 0-36)
-        if (ascending === true && descending === true) {
+        if (ascending === "On" && descending === "On") {
             let randomChoice = Math.round(Math.random());
             if (randomChoice) {
                 const n1 = Math.floor(Math.random() * (37 - interval));
@@ -135,30 +122,82 @@ function IntervalGame() {
                 setNote1(audioClips[n1].clip);
                 setNote2(audioClips[n2].clip);
             }
-        } else if (ascending === true) {
+        } else if (ascending === "On") {
             const n1 = Math.floor(Math.random() * (37 - interval));
             const n2 = n1 + interval
             setNote1(audioClips[n1].clip);
             setNote2(audioClips[n2].clip);
-        } else if (descending === true) {
+        } else if (descending === "On") {
             const n1 = Math.floor(Math.random() * (37 - interval)) + interval;
             const n2 = n1 - interval
             setNote1(audioClips[n1].clip);
             setNote2(audioClips[n2].clip);
         } else {
-            alert("You must select either ascending, or descending, or both")
+            alert("Must have A or D on...")
         }
-        
-        // console.log("Interval: ", interval);
-        // console.log("Answer: ", answer);
-        // console.log("Note 1: ", n1);
-        // console.log("Note 2: ", n2);
-        setCount(4);
-        setInputText("");
-        setGotAnswer(true);
+        //Check whether notes will be played melodically or harmonically
+        melOrHarm();
+    }
+
+    //Randomly choose melodic or harmonic if both are checked
+    //Otherwise sets to melodic or harmonic
+    function melOrHarm() {
+        if (melodic === "On" && harmonic === "On") {
+            let randomChoice = Math.round(Math.random());
+            if (randomChoice === 1) {
+                setMelodicOrHarmonic(true);
+            } else {
+                setMelodicOrHarmonic(false);
+            }
+        } else if (melodic === "On") {
+            setMelodicOrHarmonic(true);
+        } else if (harmonic === "On") {
+            setMelodicOrHarmonic(false);
+        } else {
+            alert("Must have M or H on...")
+        }
+    }
+
+
+
+    function playInterval() {
+        if (melodicOrHarmonic === true) {
+            const first_note = new Audio(note1);
+            first_note.play();
             setTimeout(() => {
-                setGotAnswer(null);
+                const second_note = new Audio(note2);
+                second_note.play();
             }, 1000);
+        } else if (melodicOrHarmonic === false) {
+            const first_note = new Audio(note1);
+            first_note.play();
+            const second_note = new Audio(note2);
+            second_note.play();
+        }
+        console.log(interval);
+        console.log(answer);
+        console.log(note1);
+        console.log(note2);
+    }
+
+    //Function for logging what is written in input
+    function handleChange(event) {
+        const newValue = event.target.value;
+        setInputText(newValue);
+    }
+
+    function check_answer() {
+        if (inputText === answer) {
+            //Get a new set of notes/interval/answers - they are chained together
+            newInterval();
+            
+            //Reset counter
+            setCount(4);
+            setInputText("");
+            setGotAnswer(true);
+                setTimeout(() => {
+                    setGotAnswer(null);
+                }, 1000);
 
         } else if (inputText !== answer) {
             setCount(count - 1);
@@ -168,67 +207,15 @@ function IntervalGame() {
                     setGotAnswer(null);
                 }, 1000);
 
-            if (count === 1) {
-                setInterval(Math.floor(Math.random() * 12) + 1)
-        if (interval === 1) {
-            setAnswer("m2");
-        } else if (interval === 2) {
-            setAnswer("M2");
-        } else if (interval === 3) {
-            setAnswer("m3");
-        } else if (interval === 4) {
-            setAnswer("M3");
-        } else if (interval === 5) {
-            setAnswer("p4");
-        } else if (interval === 6) {
-            setAnswer("tt");
-        } else if (interval === 7) {
-            setAnswer("p5");
-        } else if (interval === 8) {
-            setAnswer("m6");
-        } else if (interval === 9) {
-            setAnswer("M6");
-        } else if (interval === 10) {
-            setAnswer("m7");
-        } else if (interval === 11) {
-            setAnswer("M7");
-        } else if (interval === 12) {
-            setAnswer("p8");
-        }
-
-        //Set N1 between 0-36 (No +1 because list of sounds goes from 0-36)
-        if (ascending === true && descending === true) {
-            let randomChoice = Math.round(Math.random());
-            if (randomChoice) {
-                const n1 = Math.floor(Math.random() * (37 - interval));
-                const n2 = n1 + interval
-                setNote1(audioClips[n1].clip);
-                setNote2(audioClips[n2].clip);
-            } else if (!randomChoice) {
-                const n1 = Math.floor(Math.random() * (37 - interval)) + interval;
-                const n2 = n1 - interval
-                setNote1(audioClips[n1].clip);
-                setNote2(audioClips[n2].clip);
-            }
-        } else if (ascending === true) {
-            const n1 = Math.floor(Math.random() * (37 - interval));
-            const n2 = n1 + interval
-            setNote1(audioClips[n1].clip);
-            setNote2(audioClips[n2].clip);
-        } else if (descending === true) {
-            const n1 = Math.floor(Math.random() * (37 - interval)) + interval;
-            const n2 = n1 - interval
-            setNote1(audioClips[n1].clip);
-            setNote2(audioClips[n2].clip);
-        } else {
-            alert("You must select either ascending, or descending, or both")
-        }
-        setCount(4);
-        setInputText("");
-        setGotAnswer(false);
-            setTimeout(() => {
-                setGotAnswer(null);
-            }, 1000);
+        if (count === 1) {
+            newInterval();
+    
+            setCount(4);
+            setInputText("");
+            setGotAnswer(false);
+                setTimeout(() => {
+                    setGotAnswer(null);
+                }, 1000);
             }
         }
     }
@@ -238,14 +225,19 @@ function IntervalGame() {
         <div className={styles.container}>
             <h2 className={styles.h2}>Interval Game</h2>
             <button>Settings</button>
-                <label>Ascending</label>
+                <button id="ascending" onClick={settings} >Ascending: {ascending}</button>
+                <button id="descending" onClick={settings} >Descending: {descending}</button>
+                <button id="melodic" onClick={settings} >Melodic: {melodic}</button>
+                <button id="harmonic" onClick={settings} >Harmonic: {harmonic}</button>
+
+                {/* <label>Ascending</label>
                 <input type="checkbox" id="ascending" onChange={settings} ></input>
                 <label>Descending</label>
                 <input type="checkbox" id="descending" onChange={settings} ></input>
                 <label>Melodic</label>
                 <input type="checkbox" id="melodic" onChange={settings} ></input>
                 <label>Harmonic</label>
-                <input type="checkbox" id="harmonic" onChange={settings} ></input>
+                <input type="checkbox" id="harmonic" onChange={settings} ></input> */}
                 
 
             <button className={styles.btn} onClick={playInterval}>Play Interval</button>
@@ -255,6 +247,10 @@ function IntervalGame() {
             { gotAnswer === true ? <AiOutlineCheckCircle className={styles.correct} /> : null }
             { gotAnswer === false ? <AiOutlineCloseCircle className={styles.incorrect} /> : null }
             <Help />
+            <p>Cheat sheet:</p>
+            <p>Note 1: {note1} Note 2: {note2}</p>
+            <p>Interval: {interval} Answer: {answer}</p>
+            <p>{melodicOrHarmonic}</p>
         </div>
     );
 }
