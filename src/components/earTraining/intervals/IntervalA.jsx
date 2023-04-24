3a2905e2551767b3dc8921fe6037f3ffd64c0fce
matthew <larochelle.matthew@gmail.com>
2023-04-24 10:31:35 -0700

    complete expanded styles refactoring

--- src/components/earTraining/intervals/IntervalA.jsx
+++ src/components/earTraining/intervals/IntervalA.jsx
@@ -48,6 +48,7 @@ function IntervalA(props) {
 
     return (
         <div className={ expand ? styles.containerExpanded : styles.containerSmall}>
+
             <div className={ expand ? styles.hidden : styles.layoutSmall}>
                 <div className={styles.text} id={props.id} onClick={intervalM} >
                     <h3 className={styles.intervalSmall}>{props.interval}</h3>
@@ -55,31 +56,35 @@ function IntervalA(props) {
                 </div>
                 <AiOutlineExpand onClick={handleClick} className={styles.expandBtn}/>
             </div>
+            
             <div className={ expand ? styles.layoutExpanded : styles.hidden}>
-                <div className={styles.gridTitleButtons}>
-                    <div className={styles.pExpanded}>
-                        <p className={styles.p}>{props.interval}</p>
+                <div className={styles.expandedTitleBackground}>
+                    <div className={styles.expandedTitle}>
+                        <h2 className={styles.title}>{props.interval}</h2>
+                        <p className={styles.song}>{props.song}</p>
                     </div>
-                    <AiOutlineExpand onClick={handleClick} className={styles.expandBtn2}/>
+                </div>
+                <div className={styles.grid}>
                     <div className={styles.btnContainer}>
                         <button className={styles.btn} id={props.id} onClick={intervalM}>Melodic</button>
                         <button className={styles.btn} id={props.id} onClick={intervalH}>Harmonic</button>
                         <button className={styles.btn} id={props.id} onClick={context}>Context</button>
                     </div>
+                    <img className={styles.diagram} src={props.diagram} alt="diagram" />
+                    <iframe 
+                        width="200" 
+                        height="200" 
+                        src={props.link} 
+                        title="YouTube video player" 
+                        frameborder="0" 
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                        allowfullscreen
+                        className={styles.video}
+                        >
+                    </iframe>
                 </div>
-                <img className={styles.diagram} src={props.diagram} alt="diagram" />
-                <iframe 
-                    width="200" 
-                    height="200" 
-                    src={props.link} 
-                    title="YouTube video player" 
-                    frameborder="0" 
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-                    allowfullscreen
-                    className={styles.video}
-                    >
-                </iframe>
                 
+                <AiOutlineExpand onClick={handleClick} className={styles.expandBtn2}/>
                 
             </div>
             
