b1c6bc886a26e2d2cdd3cd8bc6635fda085d0107
matthew <larochelle.matthew@gmail.com>
2023-04-27 14:21:12 -0700

    updating server

---
+++ src/backend/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+
+//I don't know...
+const app = express();
+const port = process.env.PORT || 2977;
+
+
+//Middleware?
+app.use(cors());
+app.use(express.json());
+
+
+//Connect to MongoDB
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
+);
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+})
+
+
+
+
+//Start backend server
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
