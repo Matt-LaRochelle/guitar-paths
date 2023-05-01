16b2ed01c9a0d1b986030fb5ae47e3b5daa6a874
matthew <larochelle.matthew@gmail.com>
2023-05-01 11:28:26 -0700

    attempt 2

--- src/components/create-exercise/Create-exercise.jsx
+++ src/components/create-exercise/Create-exercise.jsx
@@ -10,35 +10,54 @@ function CreateExercise() {
         description: '',
         duration: 0,
         date: new Date(),
-        users: ['test user']
+        users: ['test user', 'test user2']
     });
 
     useEffect(() => {
         
     }, [])
 
-    function onChangeUsername(e) {
-        setExercise({
-            username: e.target.value
-        });
-    }
 
-    function onChangeDescription(e) {
-        setExercise({
-            description: e.target.value
-        });
-    }
 
-    function onChangeDuration(e) {
-        setExercise({
-            duration: e.target.value
-        });
-    }
+    function handleChange(e) {
+        const {name, value} = e.target;
 
-    function onChangeDate(date) {
-        setExercise({
-            date: date
-        });
+        setExercise((prevValue) => {
+            if (name === "username") {
+                return {
+                    username: value,
+                    description: prevValue.description,
+                    duration: prevValue.duration,
+                    date: prevValue.date,
+                    users: prevValue.users
+                }
+            } else if (name === "description") {
+                return {
+                    username: prevValue.username,
+                    description: value,
+                    duration: prevValue.duration,
+                    date: prevValue.date,
+                    users: prevValue.users
+                }
+            } else if (name === "duration") {
+                return {
+                    username: prevValue.username,
+                    description: prevValue.description,
+                    duration: value,
+                    date: prevValue.date,
+                    users: prevValue.users
+                }
+            } else if (name === "date") {
+                return {
+                    username: prevValue.username,
+                    description: prevValue.description,
+                    duration: prevValue.duration,
+                    date: value,
+                    users: prevValue.users
+                }
+            }
+            console.log(exercise)
+        })
     }
 
     function onSubmit(e) {
@@ -50,6 +69,78 @@ function CreateExercise() {
         <div className={styles.container}>
             <h3>Create New Exercise Log</h3>
             <form onSubmit={onSubmit}>
+                <div>
+                    <label>Username: </label>
+                    <select
+                        required
+                        name="username"
+                        value={exercise.username}
+                        onChange={handleChange}>
+                            {
+                                exercise.users.map((user) => {
+                                    return <option
+                                            key={user}
+                                            value={user}>{user}
+                                            </option>;
+                                })
+                            }
+                        </select>
+                </div>
+                <div>
+                    <label>Description: </label>
+                    <input type="text"
+                        required
+                        name="description"
+                        value={exercise.description}
+                        onChange={handleChange}
+                        />
+                </div>
+                <div>
+                    <label>Duration in minutes: </label> 
+                    <input
+                        type="text"
+                        name="duration"
+                        value={exercise.duration}
+                        onChange={handleChange}
+                        />
+                </div>
+                <div>
+                    <label>Date: </label>
+                    <div>
+                        <DatePicker
+                            name="date"
+                            selected={exercise.date}
+                            onChange={handleChange}
+                            />
+                    </div>
+                </div>
+                <div>
+                    <input type="submit" value="Create Exercise Log" />
+                </div>
+
+
+
+            </form>
+            
+        </div>
+    );
+}
+
+export default CreateExercise;
+
+
+
+
+
+
+
+
+
+
+
+
+
+{/* <form onSubmit={onSubmit}>
                 <div>
                     <label>Username: </label>
                     <select ref="userInput"
@@ -94,9 +185,4 @@ function CreateExercise() {
                 <div>
                     <input type="submit" value="Create Exercise Log" />
                 </div>
-            </form>
-        </div>
-    );
-}
-
-export default CreateExercise;
+            </form> */}
