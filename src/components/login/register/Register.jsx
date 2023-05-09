a1f62ef71d460de7cb2327767f17e965243d436e
matthew <larochelle.matthew@gmail.com>
2023-05-09 10:55:37 -0700

    trying to get login working

--- src/components/login/register/Register.jsx
+++ src/components/login/register/Register.jsx
@@ -89,7 +89,6 @@ function Register(props) {
                                 },
                             ]);
                         });
-                        
                     }
                 }
             }  
