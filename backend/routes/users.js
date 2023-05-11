e225777c0a40a53f268d2f710b6124055b610a4a
matthew <larochelle.matthew@gmail.com>
2023-05-11 10:18:51 -0700

    salt and hash passwords

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 let User = require('../models/user.model');
 
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 
 //First route
 router.route('/').get((req, res) => {
@@ -10,7 +13,7 @@ router.route('/').get((req, res) => {
 });
 
 
-//Handles log in functionality
+//Handles log in for existing user functionality
 router.route('/sign').post((req, res) => {
     const username = req.body.username;
     const password = req.body.password;
@@ -18,13 +21,13 @@ router.route('/sign').post((req, res) => {
     User.findOne({username: username})
         .then((docs) => {
             // console.log("result: ", docs);
-            if (docs.password === password) {
-                console.log("You good."); 
-                res.send("Password is correct.");
-            } else {
-                console.log("No good.");
-                res.send("Password is incorrect.")
-            }
+            bcrypt.compare(password, docs.password, function(err, result) {
+                if (result) {
+                    res.send("Password is correct.");
+                } else {
+                    res.send("Password is incorrect.");
+                }
+            });
         })
         .catch((err) => {
             console.log("error:", err)
@@ -32,23 +35,33 @@ router.route('/sign').post((req, res) => {
         });
 });
 
-//Handles Incoming http post requests ***FOR REGISTERING A USER***
+//Handles registering a new user functionality
 router.route('/add').post((req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
-    const birthday = req.body.birthday;
-    const email = req.body.email;
+    
 
-    const newUser = new User({
-        username,
-        password,
-        birthday,
-        email
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        // Store hash in your password DB.
+
+        const username = req.body.username;
+        const password = hash
+        const birthday = req.body.birthday;
+        const email = req.body.email;
+
+        const newUser = new User({
+            username,
+            password,
+            birthday,
+            email
+        });
+    
+        newUser.save()
+        .then(() => res.json('User added!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+
+
     });
 
-    newUser.save()
-    .then(() => res.json('User added!'))
-    .catch(err => res.status(400).json('Error: ' + err));
+    
 });
 
 module.exports = router;
